a379ff91bce0c719392e776b159f51305546f186
amir <amir11.vosoughi@gmail.com>
2023-10-31 21:27:46 +0330

    delete

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,8 +1,20 @@
+'use client'
+import { useRouter } from 'next/navigation';
 import {AiOutlineClose} from 'react-icons/ai'
+import { Router } from 'react-router-dom';
 
-const DeleteBlock = () => {
+const DeleteBlock = ({id}) => {
+  const router = useRouter()
+  const deleteTicket = async () => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}` , {
+      method : "DELETE"
+    });
+    if(res.ok) {
+      router.refresh()
+    }
+  }
   return (
-    <AiOutlineClose />
+    <AiOutlineClose  onClick={deleteTicket}/>
   )
 }
 
